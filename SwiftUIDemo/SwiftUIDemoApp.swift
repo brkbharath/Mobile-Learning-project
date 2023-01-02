69b0e5c45cdfeaac23e30ce8290edee318b8e570
brkbharath <bharath.boopathy@compass.com>
2023-01-02 21:40:11 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Bharath Raj Kumar Boopathy on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
